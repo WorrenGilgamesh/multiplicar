752413204d45282be1be24136dcffbe81d636c92
WorrenGilgamesh <victor.oz@live.com>
2016-10-17 20:30:13 -0500

    Layout junto con las actividades terminado junto con otras cosas javascript

---
+++ calculadora.js
@@ -0,0 +1,13 @@
+var Multiply = function(){
+	var number1 = document.getElementById('number1').value;
+	var number2 = document.getElementById('number2').value;
+	var text = document.getElementById('text');
+	text.innerHTML = number1*number2
+}
+
+var Divide = function(){
+	var number1 = document.getElementById('number1').value;
+	var number2 = document.getElementById('number2').value;
+	var text = document.getElementById('text');
+	text.innerHTML = number1/number2
+}
---
+++ javascript.js
@@ -0,0 +1,5 @@
+var sayHello = function(){
+	var name = document.getElementById('name').value;
+	var text = document.getElementById('text');
+	text.innerHTML = 'Hello,' + name + '!'
+}
